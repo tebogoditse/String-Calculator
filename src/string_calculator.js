1b8e67677e87764039ac51918b0ac1ba1cb4d116
Asanda <layla.radebe20@gmail.com>
2019-08-23 13:50:35 +0200

    str

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,15 +1,21 @@
 function add(str) {
 
-  var i = 0;
-  var sum = 0;
-  var find = /-?[0-9]+/g;
-  var convert = str.match(search).map(function(x){
-    return parseInt(x, 10);
-  });
+    var find = /-?[0-9]+/g;
+    var negative = []
+    var convert = str.match(find).map(function(n){
+    if (n > 1000){
+      return (0);
+    }
+    if (n < 0){
+      throw "negative numbers not allowed";
+    }
+    return parseInt(n);
+    });
 
-  for (; i < str.length; i++){
-    sum = sum + convert[i];
-  }
-
-  return sum;
+    let i = 0;
+    let sum = 0;
+    for (; i < convert.length; i++){
+      sum = sum + convert[i];
+    }
+    return sum;
 }
